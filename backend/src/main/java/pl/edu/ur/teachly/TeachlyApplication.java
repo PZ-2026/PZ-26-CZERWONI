105eede405b001bd228ad985f76f0c84522a4639
Reca <ar131498@stud.ur.edu.pl>
2026-03-25 14:08:32 +0100

    Konfiguracja struktury repozytorium, diagramy z sprintu 1

---
+++ backend/src/main/java/pl/edu/ur/teachly/TeachlyApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.ur.teachly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachlyApplication.class, args);
+	}
+
+}
