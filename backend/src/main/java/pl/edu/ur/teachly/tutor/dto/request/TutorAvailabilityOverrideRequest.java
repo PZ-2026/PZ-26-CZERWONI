4798f4de1a32d7e6174c7ecfb36ae2b15a5165c1
Adrian Raczek <ar131498@stud.ur.edu.pl>
2026-05-02 13:40:03 +0200

    Poprawki w konfiguracji checkstyle

--- backend/src/main/java/pl/edu/ur/teachly/tutor/dto/request/TutorAvailabilityOverrideRequest.java
+++ backend/src/main/java/pl/edu/ur/teachly/tutor/dto/request/TutorAvailabilityOverrideRequest.java
@@ -11,7 +11,9 @@ public record TutorAvailabilityOverrideRequest(
         LocalTime timeTo) {
     @AssertTrue(message = "Godzina zakończenia musi być późniejsza niż godzina rozpoczęcia")
     public boolean isTimeValid() {
-        if (timeFrom == null || timeTo == null) return true;
+        if (timeFrom == null || timeTo == null) {
+            return true;
+        }
         return timeTo.isAfter(timeFrom);
     }
 }
