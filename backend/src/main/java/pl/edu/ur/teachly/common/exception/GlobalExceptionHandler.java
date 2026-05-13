af98c29988a277d9f41d7952a1a425de2bf428d5
Reca <ar131498@stud.ur.edu.pl>
2026-05-13 14:50:03 +0200

    Dodanie uprawnień non-root dla folderu logs w Dockerze

--- backend/src/main/java/pl/edu/ur/teachly/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/pl/edu/ur/teachly/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package pl.edu.ur.teachly.common.exception;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -7,9 +9,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
